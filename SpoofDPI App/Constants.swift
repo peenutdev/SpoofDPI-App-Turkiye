f1f7226cab18aff410e266ab5632b13c6c338f98
kuzey <kuzey@kuzeys-MacBook-Pro.local>
2026-01-18 22:55:11 +0300

    Fix Sparkle feedURL property and add appcast.xml

--- SpoofDPI App/Constants.swift
+++ SpoofDPI App/Constants.swift
@@ -7,11 +7,16 @@ import Foundation
 
 enum Constants {
     static let defaultLanguage = Locale.SupportedLanguage.turkish
-    static let updatesCheckingFrequency: TimeInterval = 259200 // 3 days
-    
-    static let repositoryURL = URL(string: "https://github.com/KuzeyMurathan/SpoofDPI-App-Turkiye")!
-    static let actualBuildNumberURL = URL(string: "https://raw.githubusercontent.com/SpoofDPIApp/SpoofDPI-App/main/Other/ActualBuildNumber.txt")!
-    
+    static let updatesCheckingFrequency: TimeInterval = 259200  // 3 days
+
+    static let repositoryURL = URL(string: "https://github.com/peenutdev/SpoofDPI-App-Turkiye")!
+    static let appcastURL = URL(
+        string: "https://raw.githubusercontent.com/peenutdev/SpoofDPI-App-Turkiye/main/appcast.xml")!
+    static let actualBuildNumberURL = URL(
+        string:
+            "https://raw.githubusercontent.com/peenutdev/SpoofDPI-App-Turkiye/main/Other/ActualBuildNumber.txt"
+    )!
+
     static let libraryProcessNamePrefix = "spoofdpi-"
     static let libraryVersion = "0.12.0-TR"
     static let libraryParameters = "-dns-addr 77.88.8.8 -dns-port 1253 -window-size 5"
--- SpoofDPI App/Framework/Services/UpdateService.swift
+++ SpoofDPI App/Framework/Services/UpdateService.swift
@@ -2,87 +2,119 @@
 //  UpdateService.swift
 //  SpoofDPI App
 //
+//
 
 import AppKit
+import SwiftUI
 
-final class UpdateService: ObservableObject {
+#if canImport(Sparkle)
+    import Sparkle
+#endif
+
+final class UpdateService: NSObject, ObservableObject {
     static let instance = UpdateService()
-    
+
     private lazy var settingsService = SettingsService.instance
     private lazy var windowService = WindowService.instance
-    
-    private init() {
-        guard !ProcessInfo.isPreview else {
-            return
-        }
-        
-        DispatchQueue.main.asyncAfter(
-            deadline: .now() + 1
-        ) {
-            self.checkAvailability()
-        }
-        
-        let timer = Timer.scheduledTimer(
-            withTimeInterval: Constants.updatesCheckingFrequency,
-            repeats: true
-        ) { [weak self] _ in
-            self?.checkAvailability()
-        }
-        
-        RunLoop.main.add(timer, forMode: .common)
+
+    #if canImport(Sparkle)
+        private var updaterController: SPUStandardUpdaterController!
+    #endif
+
+    override init() {
+        super.init()
+        #if canImport(Sparkle)
+            let controller = SPUStandardUpdaterController(
+                startingUpdater: true, updaterDelegate: self, userDriverDelegate: nil)
+            self.updaterController = controller
+        #else
+            guard !ProcessInfo.isPreview else {
+                return
+            }
+
+            DispatchQueue.main.asyncAfter(
+                deadline: .now() + 1
+            ) {
+                self.checkAvailability()
+            }
+
+            let timer = Timer.scheduledTimer(
+                withTimeInterval: Constants.updatesCheckingFrequency,
+                repeats: true
+            ) { [weak self] _ in
+                self?.checkAvailability()
+            }
+
+            RunLoop.main.add(timer, forMode: .common)
+        #endif
     }
-    
+
     func checkAvailability() {
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.timeoutIntervalForRequest = 10
-        
-        let session = URLSession(configuration: configuration)
-        
-        session.dataTask(with: Constants.actualBuildNumberURL) { [weak self] data, _, _ in
+        #if canImport(Sparkle)
+            updaterController.checkForUpdates(nil)
+        #else
+            let configuration = URLSessionConfiguration.ephemeral
+            configuration.timeoutIntervalForRequest = 10
+
+            let session = URLSession(configuration: configuration)
+
+            session.dataTask(with: Constants.actualBuildNumberURL) { [weak self] data, _, _ in
+                guard
+                    let self,
+                    let data,
+                    let text = String(data: data, encoding: .utf8),
+                    let actualBuildNumber = Int(
+                        text.trimmingCharacters(in: .whitespacesAndNewlines))
+                else {
+                    return
+                }
+
+                DispatchQueue.main.async {
+                    self.settingsService.latestKnownActualBuildNumber = actualBuildNumber
+                    self.showAlertIfNeeded()
+                }
+            }.resume()
+        #endif
+    }
+
+    #if !canImport(Sparkle)
+        private func showAlertIfNeeded() {
             guard
-                let self,
-                let data,
-                let text = String(data: data, encoding: .utf8),
-                let actualBuildNumber = Int(text.trimmingCharacters(in: .whitespacesAndNewlines))
+                let currentBuildNumber = Bundle.main.buildNumber,
+                currentBuildNumber < settingsService.latestKnownActualBuildNumber
             else {
                 return
             }
-            
-            DispatchQueue.main.async {
-                self.settingsService.latestKnownActualBuildNumber = actualBuildNumber
-                self.showAlertIfNeeded()
+
+            windowService.isMainWindowVisible = true
+
+            let alert = NSAlert().with {
+                typealias LocalizedString = SpoofDPI_App.LocalizedString.Updates.Alert
+
+                $0.messageText = LocalizedString.title(appName: Bundle.main.name)
+                $0.informativeText = LocalizedString.description
+
+                $0.addButton(withTitle: LocalizedString.Buttons.update)
+                $0.addButton(withTitle: LocalizedString.Buttons.close)
+
+                $0.alertStyle = .warning
             }
-        }.resume()
-    }
-    
-    private func showAlertIfNeeded() {
-        guard
-            let currentBuildNumber = Bundle.main.buildNumber,
-            currentBuildNumber < settingsService.latestKnownActualBuildNumber
-        else {
-            return
-        }
-        
-        windowService.isMainWindowVisible = true
-        
-        let alert = NSAlert().with {
-            typealias LocalizedString = SpoofDPI_App.LocalizedString.Updates.Alert
-            
-            $0.messageText = LocalizedString.title(appName: Bundle.main.name)
-            $0.informativeText = LocalizedString.description
-            
-            $0.addButton(withTitle: LocalizedString.Buttons.update)
-            $0.addButton(withTitle: LocalizedString.Buttons.close)
-            
-            $0.alertStyle = .warning
-        }
-        
-        switch alert.runModal() {
+
+            switch alert.runModal() {
             case .alertFirstButtonReturn:
                 NSWorkspace.shared.open(Constants.repositoryURL)
-                
+
             default:
                 break
+            }
+        }
+    #endif
+}
+
+#if canImport(Sparkle)
+    extension UpdateService: SPUUpdaterDelegate {
+        func feedURLString(for updater: SPUUpdater) -> String? {
+            return Constants.appcastURL.absoluteString
         }
     }
-}
+#endif

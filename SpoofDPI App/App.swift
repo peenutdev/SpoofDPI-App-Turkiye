cd87fd961437eab5ca84e31ab44a67b8f149e650
kuzey <kuzey@kuzeys-MacBook-Pro.local>
2026-01-18 22:35:16 +0300

    Fix app crash on startup items auto start

--- SpoofDPI App/App.swift
+++ SpoofDPI App/App.swift
@@ -7,27 +7,29 @@ import SwiftUI
 @main
 struct App: SwiftUI.App {
     @NSApplicationDelegateAdaptor(Delegate.self) private var delegate
-    
+
     var body: some Scene {
         MainScene()
     }
 }
 
-private extension App {
-    final class Delegate: NSObject, NSApplicationDelegate {
+extension App {
+    fileprivate final class Delegate: NSObject, NSApplicationDelegate {
         func applicationDidFinishLaunching(_ notification: Notification) {
-            
-            _ = AutoLaunchService.instance
-            _ = MenuBarIconService.instance
-            _ = ProtectionService.instance
-            _ = UpdateService.instance
-            _ = WindowService.instance
+
+            DispatchQueue.main.async {
+                _ = AutoLaunchService.instance
+                _ = MenuBarIconService.instance
+                _ = ProtectionService.instance
+                _ = UpdateService.instance
+                _ = WindowService.instance
+            }
         }
-        
+
         func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
             return false
         }
-        
+
         func applicationWillTerminate(_ notification: Notification) {
             ProtectionService.instance.prepareForTermination()
         }
--- SpoofDPI App/Framework/Services/AutoLaunchService.swift
+++ SpoofDPI App/Framework/Services/AutoLaunchService.swift
@@ -8,34 +8,36 @@ import ServiceManagement
 
 final class AutoLaunchService {
     static let instance = AutoLaunchService()
-    
+
     private lazy var settingsService = SettingsService.instance
     private lazy var windowService = WindowService.instance
-    
+
     private var isEnabledObservation: AnyCancellable?
-    
+
     private init?() {
         guard !ProcessInfo.isPreview else {
             return nil
         }
-        
+
         isEnabledObservation = settingsService.$isAutomaticLaunchEnabled.sink {
             let appService = SMAppService.mainApp
-            
+
             if $0 {
                 try? appService.register()
             } else {
                 try? appService.unregister()
             }
         }
-        
+
         if let appleEvent = NSAppleEventManager.shared().currentAppleEvent,
-           appleEvent.eventID == kAEOpenApplication,
-           
-           let parameterDescriptor = appleEvent.paramDescriptor(forKeyword: keyAEPropData),
-           parameterDescriptor.enumCodeValue == keyAELaunchedAsLogInItem
+            appleEvent.eventID == kAEOpenApplication,
+
+            let parameterDescriptor = appleEvent.paramDescriptor(forKeyword: keyAEPropData),
+            parameterDescriptor.enumCodeValue == keyAELaunchedAsLogInItem
         {
-            windowService.isMainWindowVisible = false
+            DispatchQueue.main.async {
+                self.windowService.isMainWindowVisible = false
+            }
         }
     }
 }
